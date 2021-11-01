92e6e22117c316810cc650e0157d42438d175db5
Shadowda <piercej2000@gmail.com>
2021-11-01 09:36:56 -0400

    database and test leaflet intigration

--- Aegis/models.py
+++ Aegis/models.py
@@ -1,7 +1,20 @@
 from Aegis import db
 
-# Table of items
-class Item(db.Model):
-	name = db.Column(db.String(50), nullable=False)
-	id = db.Column(db.Integer, primary_key=True)
-	points = db.Column(db.Integer)
+class State(db.Model):
+	state_id = db.Column(db.Integer, primary_key=True)
+	json = db.Column(db.Text)
+
+class County(db.Model):
+	fips = db.Column(db.Integer, primary_key=True)
+	json = db.Column(db.Text)
+	
+class Vaccination(db.Model):
+	fips = db.Column(db.Integer, db.ForeignKey('County.fips'), primary_key=True,)
+	date = db.Column(db.Date, primary_key=True)
+	full_vax = db.Column(db.Integer)
+
+class Infected(db.Model):
+	fips = db.Column(db.Integer, db.ForeignKey('County.fips'), primary_key=True)
+	date = db.Column(db.Date, primary_key=True)
+	cases = db.Column(db.Integer)
+	deaths = db.Column(db.Integer)
--- Aegis/routes.py
+++ Aegis/routes.py
@@ -1,9 +1,17 @@
 from flask import render_template, url_for, flash, redirect, request
 from sqlalchemy.exc import IntegrityError
 from Aegis import app, db
-from Aegis.models import Item
+from Aegis.models import State
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-
-	return render_template('main.html', title='Individual Assignment')
+	data = "hello"
+	if request.method == 'GET':
+		data = request.args.get('state')
+		print(data)
+	return render_template('Choropleth Tutorial - Leaflet.html', title='Individual Assignment', data=data)
+	
+#@app.route('/<_data>', methods=['GET', 'POST'])
+#def data(_data):
+#		print(_data)
+#helpers
