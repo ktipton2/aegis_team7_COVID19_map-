7c9ee3ada980410effabe9d79ad5decbebbc2e9d
Shadowda <piercej2000@gmail.com>
2021-11-03 15:42:55 -0400

    update stuff

--- Aegis/models.py
+++ Aegis/models.py
@@ -1,21 +1,23 @@
 from Aegis import db
 
-##class State(db.Model):
-#	state_id = db.Column(db.Integer, primary_key=True)
-#	json = db.Column(db.Text)
+class State(db.Model):
+	state_id = db.Column(db.Integer, primary_key=True)
+	json = db.Column(db.Text)
 
 class County(db.Model):
-	fips = db.Column(db.Integer, primary_key=True)
+	fips = db.Column(db.String(5), primary_key=True)
 	state = db.Column(db.Integer)
 	json = db.Column(db.Text)
 	
 class Vaccination(db.Model):
-	fips = db.Column(db.Integer, db.ForeignKey('County.fips'), primary_key=True,)
+	fips = db.Column(db.String(5), db.ForeignKey('county.fips'), primary_key=True)
+	state = db.Column(db.String(2))
 	date = db.Column(db.Date, primary_key=True)
 	full_vax = db.Column(db.Integer)
 
 class Infected(db.Model):
-	fips = db.Column(db.Integer, db.ForeignKey('County.fips'), primary_key=True)
+	fips = db.Column(db.String(5), db.ForeignKey('county.fips'), primary_key=True)
+	state = db.Column(db.String(2))
 	date = db.Column(db.Date, primary_key=True)
 	cases = db.Column(db.Integer)
 	deaths = db.Column(db.Integer)
--- Aegis/routes.py
+++ Aegis/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, url_for, flash, redirect, request, jsonify
 from sqlalchemy.exc import IntegrityError
 from Aegis import app, db
-from Aegis.models import County
+from Aegis.models import Vaccination, County, Infected, State
+from datetime import date
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -13,5 +14,22 @@ def main():
 	
 @app.route('/geojson-features', methods=['GET'])
 def get_all_points():
-    a = request.args.get('state')
-    return jsonify(a)
+	a = request.args.get('state')
+	data = Vaccination.query.filter_by(state=a).all()
+	result = ""
+	for row in data:
+		result += row.fips
+	return jsonify(result)
+
+   
+   
+#fips = db.Column(db.Integer, db.ForeignKey('County.fips'), #primary_key=True,)
+#	date = db.Column(db.Date, primary_key=True)
+#	full_vax = db.Column(db.Integer)
+
+#db.create_all()
+
+#vax = Vaccination(fips='00000', state='48', date=date.fromisoformat('2019-12-04'), full_vax=5) #create the user for the db
+#db.session.add(vax) #stage the user for the db
+#db.session.commit() #commit new user to db
+
