50dd665eb3a26a5754abfbfe93c62c2a2b56464d
Shadowda <piercej2000@gmail.com>
2021-11-05 13:13:50 -0400

    11:00 meeting

--- Aegis/models.py
+++ Aegis/models.py
@@ -2,22 +2,23 @@ from Aegis import db
 
 class State(db.Model):
 	state_id = db.Column(db.Integer, primary_key=True)
-	json = db.Column(db.Text)
+	#json = db.Column(db.Text)
 
 class County(db.Model):
 	fips = db.Column(db.String(5), primary_key=True)
 	state = db.Column(db.Integer)
-	json = db.Column(db.Text)
+	#json = db.Column(db.Text)
 	
 class Vaccination(db.Model):
 	fips = db.Column(db.String(5), db.ForeignKey('county.fips'), primary_key=True)
 	state = db.Column(db.String(2))
-	date = db.Column(db.String(10), primary_key=True)
-	full_vax = db.Column(db.Integer)
+	date = db.Column(db.Date, primary_key=True)
+	full_vax = db.Column(db.Integer) # Series_Complete_Yes Total number of people who are fully vaccinated 
+	percentage = db.Column(db.Integer) # Percent of people who are fully vaccinated
 
 class Infected(db.Model):
 	fips = db.Column(db.String(5), db.ForeignKey('county.fips'), primary_key=True)
 	state = db.Column(db.String(2))
-	date = db.Column(db.String(10), primary_key=True)
+	date = db.Column(db.Date, primary_key=True)
 	cases = db.Column(db.Integer)
 	deaths = db.Column(db.Integer)
--- Aegis/pulldata.py
+++ Aegis/pulldata.py
@@ -13,16 +13,38 @@ list_NYT = requests.get(counties_url).text.split('\n')
 # dict json.
 json_CDC = requests.get(cdc_url).json()
 
-print(json_CDC[-1]['date'][0:10])
+#print(json_CDC[-1]['administered_dose1_recip_12pluspop_pct'])
 
 #date,county,state,fips,cases,deaths
-print(list_NYT[-1].split(','))
+#print(list_NYT[-1].split(','))
 	
+#3283 counties?
 for item in json_CDC[:-10:-1]:
-		print(item['date'][0:10])
+	if item['fips'] != "UNK":
+		print([item['date'][0:10], item['fips'], item['series_complete_yes'] ])
+		#new_vax = Vaccination(fips=item['fips'], state=item['fips'][0:2], date=item['date'][0:10], full_vax=item['series_complete_yes'], percentage=item['series_complete_pop_pct'])
+		#print(item['date'][0:10])
+		#print(item['fips'])
+		#print(item['series_complete_yes'])
 
 for item in list_NYT[:-10:-1]:
 		print(item.split(','))
+
+
+ROC = ((Q['series_complete_yes'] /  Q2['series_complete_yes']) -1) * 100
+
+X per time scale.
+per day, per week.
+
+rate = Q['series_complete_yes'] -  Q2['series_complete_yes']) / days
+
+new cases per timefram
+#def get_or_create(cls, name):
+#    exists = db.session.query(Employee.id).filter_by(name=name).scalar() is not None
+#    if exists:
+#        return db.session.query(Employee).filter_by(name=name).first()
+#    return cls(name=name)
+
 #new_vax = Vaccination(fips='00000', state='48', date=date.fromisoformat('2019-12-04'), full_vax=5)
 #new_county = County 
 #new_infected = Infected
